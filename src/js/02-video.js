0f10fdec33d665a2b3f10c119526b55e6f73493d
Mykita Yampol <jampol20003@gmail.com>
2023-04-17 18:31:57 +0300

    2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+
+const iframe = document.querySelector('iframe');
+
+player.on('timeupdate', throttle(function(event) {
+
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000));
+
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
